da3e9337a96913ac01166fa03fd843b92fb25a0e
Sean Michael Amador <amador.sean@gmail.com>
2015-07-30 20:32:37 +0800

    index fix

--- includes/footer.php
+++ includes/footer.php
@@ -1,13 +1,5 @@
 <?php
 echo'
-<script src="assets/js/core.js"></script>
-<script src="js/jquery-2.1.1.js"></script>
-<script src="assets/js/vendor/jquery.js"></script>
-    <script src="assets/js/foundation.min.js"></script>
-    <script>
-      $(document).foundation();
-    </script>
-</body>
-</html>';
+';
 ?>
 	
