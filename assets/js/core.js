133548b863cbd187eb2184e6c8ae308518ee942b
Sean Michael Amador <amador.sean@gmail.com>
2015-08-07 22:16:55 +0800

    js fix

--- assets/js/core.js
+++ assets/js/core.js
@@ -24,62 +24,12 @@ jQuery(window).load(function () {
 });
 
 
-jQuery(document).ready(function($){
-	var w,mHeight,tests=$("#testimonials");
-	w=tests.outerWidth();
-	mHeight=0;
-	tests.find(".testimonial").each(function(index){
-		$("#t_pagers").find(".pager:eq(0)").addClass("active");	//make the first pager active initially
-		if(index==0)
-			$(this).addClass("active");	//make the first slide active initially
-		if($(this).height()>mHeight)	//just finding the max height of the slides
-			mHeight=$(this).height();
-		var l=index*w;				//find the left position of each slide
-		$(this).css("left",l);			//set the left position
-		tests.find("#test_container").height(mHeight);	//make the height of the slider equal to the max height of the slides
-	});
-	$(".pager").on("click",function(e){	//clicking action for pagination
-		e.preventDefault();
-		next=$(this).index(".pager");
-		clearInterval(t_int);	//clicking stops the autoplay we will define later
-		moveIt(next);
-	});
-	var t_int=setInterval(function(){	//for autoplay
-		var i=$(".testimonial.active").index(".testimonial");
-		if(i==$(".testimonial").length-1)
-			next=0;
-		else
-			next=i+1;
-		moveIt(next);
-	},3000);
-});
-function moveIt(next){	//the main sliding function
-	var c=parseInt($(".testimonial.active").removeClass("active").css("left"));	//current position
-	var n=parseInt($(".testimonial").eq(next).addClass("active").css("left"));	//new position
-	$(".testimonial").each(function(){	//shift each slide
-		if(n>c)
-			$(this).animate({'left':'-='+(n-c)+'px'});
-		else
-			$(this).animate({'left':'+='+Math.abs(n-c)+'px'});
-	});
-	$(".pager.active").removeClass("active");	//very basic
-	$("#t_pagers").find(".pager").eq(next).addClass("active");	//very basic
-}
+
 
 $(".expand-about").click(function () {
     $(".about-page-content-2").css ("display","inline-block");
     $(".about-page-content-3").css ("display","inline-block");
 });
-
-// $(window).scroll(function() {
-// if ($(this).scrollTop() > 1){  
-//     $('header').addClass("sticky");
-//   }
-//   else{
-//     $('header').removeClass("sticky");
-//   }
-// });
-
 
 
 $(function() {
