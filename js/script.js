7fa5b217003735c7255e90c317164b55a9f6030e
Sean Michael Amador <amador.sean@gmail.com>
2017-06-08 02:35:15 +0800

    hover effects

--- js/script.js
+++ js/script.js
@@ -6,43 +6,44 @@
 //   TweenMax.to(this, .5, {borderLeft: "10px solid #00b89d"})
 // })
 
-var tm = new TweenMax({ });
-var tl = new TimelineMax({ });
+var tm = new TweenMax({});
+var tl = new TimelineMax({});
 var win = jQuery(window);
 
-$(document).ready(function() { 
-	tl.set('.film',{className:"+=hide"})
-	menu();
-	smoothScroll();
-	// tl.set('.wrapper',{scale: 0.9, opacity: 0})
- });
+$(document).ready(function() {
+    tl.set('.film', { className: "+=hide" })
+    menu();
+    smoothScroll();
+    
+    // tl.set('.wrapper',{scale: 0.9, opacity: 0})
+});
 $(window).bind("load", function() {
-	$('.wrapper').removeClass('hide');
-	// tl.to('.wrapper',1,{scale: 1, opacity: 1, ease: Expo.easeInOut});
-	$('.loader').hide();
+    $('.wrapper').removeClass('hide');
+    // tl.to('.wrapper',1,{scale: 1, opacity: 1, ease: Expo.easeInOut});
+    $('.loader').hide();
 });
 
 
-function menu(){
-	var dev = $('.content-selector .devNav');
-	var film = $('.content-selector .filmNav');
+function menu() {
+    var dev = $('.content-selector .devNav');
+    var film = $('.content-selector .filmNav');
 
-	dev.click(function(){
-		$('.film').removeClass('show').addClass('hide');
-		$('.developer').removeClass('hide').addClass('show');
-		dev.addClass('active');
-		film.removeClass('active');
-		console.log('dev');
+    dev.click(function() {
+        $('.film').removeClass('show').addClass('hide');
+        $('.developer').removeClass('hide').addClass('show');
+        dev.addClass('active');
+        film.removeClass('active');
+        console.log('dev');
 
-	});
+    });
 
-	film.click(function(){
-		$('.developer').removeClass('show').addClass('hide');
-		$('.film').removeClass('hide').addClass('show');
-		film.addClass('active');
-		dev.removeClass('active');
-		console.log('film');
-	});
+    film.click(function() {
+        $('.developer').removeClass('show').addClass('hide');
+        $('.film').removeClass('hide').addClass('show');
+        film.addClass('active');
+        dev.removeClass('active');
+        console.log('film');
+    });
 }
 
 // smooth scroll fn
@@ -52,24 +53,24 @@ function smoothScroll() {
 
     win.on("mousewheel DOMMouseScroll", function(event) {
         event.preventDefault();
-        
 
-            // var delta = event.originalEvent.wheelDelta / 120 || -event.originalEvent.detail / 3;
-            var delta = ( Math.abs(event.originalEvent.wheelDelta) < 120 ? event.originalEvent.wheelDelta / 6 : event.originalEvent.wheelDelta / 120 ) || -event.originalEvent.detail;
 
-            // console.log('wheelDelta: ' + event.originalEvent.wheelDelta);
-            // console.log('detail: ' + -event.originalEvent.detail);
-            // console.log('delta: ' + delta);
-            var scrollTop = win.scrollTop();
-            var finalScroll = scrollTop - parseInt(delta * scrollDistance);
+        // var delta = event.originalEvent.wheelDelta / 120 || -event.originalEvent.detail / 3;
+        var delta = (Math.abs(event.originalEvent.wheelDelta) < 120 ? event.originalEvent.wheelDelta / 6 : event.originalEvent.wheelDelta / 120) || -event.originalEvent.detail;
 
-            TweenMax.to(win, scrollTime, {
-                scrollTo: { y: finalScroll, autoKill: true },
-                ease: Power1.easeOut,
-                autoKill: true,
-                overwrite: 5
-            });
-        
+        // console.log('wheelDelta: ' + event.originalEvent.wheelDelta);
+        // console.log('detail: ' + -event.originalEvent.detail);
+        // console.log('delta: ' + delta);
+        var scrollTop = win.scrollTop();
+        var finalScroll = scrollTop - parseInt(delta * scrollDistance);
+
+        TweenMax.to(win, scrollTime, {
+            scrollTo: { y: finalScroll, autoKill: true },
+            ease: Power1.easeOut,
+            autoKill: true,
+            overwrite: 5
+        });
+
     });
 
 }
