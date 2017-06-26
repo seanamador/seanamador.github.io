8c7a261b3cc52b2970a71845a809f3c329b73079
Sean Michael Amador <amador.sean@gmail.com>
2017-06-26 17:55:51 +0800

    update layout

--- js/script.js
+++ js/script.js
@@ -107,6 +107,17 @@ function whatido() {
 
 }
 
+ var body = document.body,timer;
+window.addEventListener('scroll', function() {
+   clearTimeout(timer);
+  if(!body.classList.contains('disable-hover')) {
+    body.classList.add('disable-hover')
+  }
+   timer = setTimeout(function(){
+    body.classList.remove('disable-hover')
+  },500);
+}, false);
+
 // $(window).scroll(function() {
 //  var distance = $('.scroll-detect').offset().top,
 //         $window = $(window);
